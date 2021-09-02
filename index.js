9669ea2b0f3b8f4d03f2a24dd6c6a000b14c06f8
Franky777shg <frengky.sihombing.777@gmail.com>
2021-09-02 11:36:14 +0700

    add feature login with google

--- index.js
+++ index.js
@@ -15,4 +15,8 @@ server.get('/user', (req, res) => {
     res.status(200).send('<h1>API Practice with CI/CD Route User :) </h1>')
 })
 
+server.get('/login', (req, res) => {
+    res.status(200).send('<h1>API Practice with CI/CD Login with Google :) </h1>')
+})
+
 server.listen(PORT, () => console.log(`Server is Running at PORT : ${PORT}`))
